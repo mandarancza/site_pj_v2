8cdbd5c4b031de57ef4114f16af57101f0267915
mandarancza <tenisremis@gmail.com>
2021-07-03 20:33:23 +0200

    pierdoły

--- sidenav.js
+++ sidenav.js
@@ -1,9 +1,22 @@
+var zmienna = 0;
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("all").setAttribute("onclick", "allClose()");
+}
+  
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("all").removeAttribute("onclick");
+} 
+
+function allClose(){
+  zmienna++;
+  if(zmienna == 2){
+    zmienna = 0;
+    closeNav();
   }
   
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  } 
+}
